589d998edb39c2aecc737bc4bd1c4b20b55c2d59
david zhang <zhangdapeng89@126.com>
2013-06-21 12:29:18 +0800

    重新提交
    重新提交

---
+++ admin/nav.php
@@ -0,0 +1,47 @@
+<div class="navbar navbar-inverse">
+    <div class="navbar-inner">
+        <div class="container">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="nav-collapse collapse">
+                <a class="brand" href="index.php">网站名称 - 后台管理</a>
+                <ul class="nav">
+                    <li class="<?php echo preg_match('#blog#i',basename($_SERVER['PHP_SELF'])) ? 'active' : ''; ?>">
+                        <a class="nav-item" href="blog.php">
+                        博文管理
+                        </a>
+                    </li>
+                    <li class="<?php echo preg_match('#category#i',basename($_SERVER['PHP_SELF'])) ? 'active' : ''; ?>">
+                        <a class="nav-item" href="category.php">
+                        分类管理
+                        </a>
+                    </li>
+                    <li class="<?php echo preg_match('#file#i',basename($_SERVER['PHP_SELF'])) ? 'active' : ''; ?>">
+                        <a class="nav-item" href="file.php">
+                        文件管理
+                        </a>
+                    </li>
+                    <li class="<?php echo preg_match('#link#i',basename($_SERVER['PHP_SELF'])) ? 'active' : ''; ?>">
+                        <a class="nav-item" href="link.php">
+                        链接管理
+                        </a>
+                    </li>
+                    <li class="<?php echo preg_match('#user#i',basename($_SERVER['PHP_SELF'])) ? 'active' : ''; ?>">
+                        <a class="nav-item" href="user.php">
+                        用户管理
+                        </a>
+                    </li>
+                    <li class="">
+                        <a class="nav-item" href="user.php">
+                        退出（davidzhang）
+                        </a>
+                    </li>
+                </ul>
+            </div><!--/.nav-collapse -->
+
+        </div>
+    </div>
+</div>
---
+++ container_404.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <h1>未找到 <span>:(</span></h1>
+    <p class="lead">很抱歉，你想查看的页面不存在.</p>
+    <p class="lead">它看起来像这样的结果:</p>
+    <ul class="nav nav-list">
+        <li><a href="#"><i class="icon-chevron-right"></i> 输入了错误的地址</a></li>
+        <li class=""><a href="#"><i class="icon-chevron-right"></i> 一个过期的链接</a></li>
+    </ul>
+    <script>
+        var GOOG_FIXURL_LANG = (navigator.language || '').slice(0,2),GOOG_FIXURL_SITE = location.host;
+    </script>
+    <script src="http://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+</div>
