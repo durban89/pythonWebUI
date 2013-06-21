786207faeb75cb8ba22effeaa11be9a5e6e51771
david zhang <zhangdapeng89@126.com>
2013-06-21 12:24:57 +0800

    Revert "创建提交"
    This reverts commit 0191eaa4b60453113507b1d53eaee758962a8b62.

--- admin/loader_styles.php
+++
@@ -1,7 +0,0 @@
-<!-- styles -->
-<link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
-<link href="../bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
-<link href="../css/flat-ui.css" rel="stylesheet">
-<link href="../css/flatui-docs.css" rel="stylesheet">
-<link href="../bootstrap/css/prettify.css" rel="stylesheet">	
-<link href="../css/themes/tab.css" rel="stylesheet">
--- header.php
+++
@@ -1,6 +0,0 @@
-<header class="jumbotron subhead" id="overview">
-  <div class="container">
-    <h1>网站名称</h1>
-    <p class="lead">网站的简单介绍</p>
-  </div>
-</header>
